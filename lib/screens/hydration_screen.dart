490718a934286bb2cc3df4cda54c9789aa802c62
WeronikaWitasik <40548953+WeronikaWitasik@users.noreply.github.com>
2021-12-27 16:58:20 +0100

    added animation to progress bar

--- lib/screens/hydration_screen.dart
+++ lib/screens/hydration_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 
@@ -10,6 +12,30 @@ class HydrationScreen extends StatefulWidget {
 
 class _HydrationScreenState extends State<HydrationScreen> {
   int _counter = 0;
+  double progressValue = 1;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    if (mounted) {
+      _timer = Timer.periodic(const Duration(milliseconds: 20), (Timer _timer) {
+        setState(() {
+          if (progressValue == 100) {
+            progressValue = 0;
+          } else {
+            progressValue++;
+          }
+        });
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -32,18 +58,27 @@ class _HydrationScreenState extends State<HydrationScreen> {
             RadialAxis(
               showLabels: false,
               showTicks: false,
-              startAngle: 180,
-              endAngle: 0,
-              radiusFactor: 0.7,
-              // canScaleToFit: true,
+              minimum: 0,
+              maximum: 100,
+              // startAngle: 180,
+              // endAngle: 0,
+              radiusFactor: 0.8,
               axisLineStyle: const AxisLineStyle(
                 thickness: 0.1,
                 color: Color.fromARGB(30, 0, 169, 181),
                 thicknessUnit: GaugeSizeUnit.factor,
                 cornerStyle: CornerStyle.startCurve,
               ),
-              pointers: const <GaugePointer>[
-                RangePointer(value: 0, width: 0.1, sizeUnit: GaugeSizeUnit.factor, cornerStyle: CornerStyle.bothCurve)
+              pointers: <GaugePointer>[
+                RangePointer(
+                  value: progressValue,
+                  width: 0.1,
+                  sizeUnit: GaugeSizeUnit.factor,
+                  cornerStyle: CornerStyle.bothCurve,
+                  enableAnimation: true,
+                  animationDuration: 20,
+                  animationType: AnimationType.linear,
+                )
               ],
               annotations: [
                 GaugeAnnotation(
