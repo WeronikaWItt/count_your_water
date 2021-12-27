59c132c1d65445fef52175108a3ad8da5c722598
WeronikaWitasik <40548953+WeronikaWitasik@users.noreply.github.com>
2021-12-27 13:05:16 +0100

    added static hydration history card

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Column(
+        children: [
+          Card(
+            elevation: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text('27.12.2021'),
+                  Text('1500/1800 ml'),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
