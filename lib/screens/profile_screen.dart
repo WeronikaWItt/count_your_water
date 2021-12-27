4357f3bafffdcf415569708fbc7d8ea987fe9d16
WeronikaWitasik <40548953+WeronikaWitasik@users.noreply.github.com>
2021-12-27 13:28:52 +0100

    added static form

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, left: 10, right: 10),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text('Płeć'),
+            MaterialButton(
+              onPressed: () {},
+              padding: const EdgeInsets.all(15),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0), side: const BorderSide(color: Colors.blue)),
+              child: const Text('Kobieta'),
+            ),
+            const SizedBox(height: 10),
+            const Text('Waga (kg)'),
+            const SizedBox(height: 5),
+            const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
+                    ),
+                  )),
+            ),
+            const SizedBox(height: 10),
+            const Text('Wiek'),
+            const SizedBox(height: 5),
+            const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
+                    ),
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
