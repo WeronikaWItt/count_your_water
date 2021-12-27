3070a82817931c003941325659ba8b49ee786d95
WeronikaWitasik <40548953+WeronikaWitasik@users.noreply.github.com>
2021-12-27 13:02:02 +0100

    added quotes card and rounded border

--- lib/screens/hydration_screen.dart
+++ lib/screens/hydration_screen.dart
@@ -25,32 +25,56 @@ class _HydrationScreenState extends State<HydrationScreen> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        const Text(
-          'You have drunk this many glasses today:',
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 80, vertical: 45),
+          padding: const EdgeInsets.all(40),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(250), border: Border.all(width: 10, color: Colors.blue)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Cel dnia',
+              ),
+              Text(
+                '$_counter/1800ml',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                    elevation: 3,
+                    onPressed: _decrementCounter,
+                    child: const Icon(Icons.minimize_sharp),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  FloatingActionButton(
+                    elevation: 3,
+                    onPressed: _incrementCounter,
+                    child: const Icon(Icons.add),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
-        Text(
-          '$_counter',
-          style: Theme.of(context).textTheme.headline4,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FloatingActionButton(
+        GestureDetector(
+          child: const SizedBox(
+            width: 300,
+            child: Card(
               elevation: 3,
-              onPressed: _decrementCounter,
-              child: const Icon(Icons.minimize_sharp),
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text('Bądź jak woda – przystosowuje się do każdego naczynia, a może zniszczyć skałę.'),
+              ),
             ),
-            const SizedBox(
-              width: 20,
-            ),
-            FloatingActionButton(
-              elevation: 3,
-              onPressed: _incrementCounter,
-              child: const Icon(Icons.add),
-            ),
-          ],
+          ),
         )
       ],
     );
