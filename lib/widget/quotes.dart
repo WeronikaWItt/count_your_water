93192e0591b4b71d8a32f5f16dd4f16fb761b21e
WeronikaWitasik <40548953+WeronikaWitasik@users.noreply.github.com>
2022-01-13 15:25:53 +0100

    code for random quotes

---
+++ lib/widget/quotes.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+class QuoteList {
+  List<String> quotes = [
+    'Bądź jak woda – przystosowuje się do każdego naczynia, a może zniszczyć skałę.',
+    'Ludzie są jak morze, czasem łagodni i przyjaźni, czasem burzliwi i zdradliwi. Przede wszystkim to jednak tylko woda.',
+    'Żyjemy niczym woda spływająca z góry, podążając mniej więcej w tym samym kierunku, póki jakaś przeszkoda nie zmusi nas do poszukania nowego koryta.',
+    'Bądź niczym bijące źródło, nie zaś jak staw, w którym zawsze stoi ta sama woda.',
+    'Chcesz być zdrowy, młody, nie stroń nigdy od wody.',
+    'Wszystkie istoty ciągną do wody i wokół niej organizują swoje życie. Podobnie ludzkość. Sercem każdej społeczności jest woda: wielka jak morze albo mała jak źródełko.',
+    'Woda pokrywa 70,9 procent powierzchni planety.',
+    'W atmosferze jest więcej wody niż we wszystkich naszych rzekach razem wziętych.',
+    'Meduza i ogórek to w 95% woda.',
+    'Woda jest najważniejszym zasobem na świecie.',
+    'Czasami gorąca woda może zamarznąć szybciej niż zimna woda, jest to tzw. efekt Mpemba',
+    'Woda składa się z dwóch pierwiastków: wodoru i tlenu. 2 wodory + 1 tlen = H2O.',
+    'NASA odkryła wodę w formie lodu na księżycu.',
+    'Woda jest drugą naczystszą cząsteczką we wszechświecie. Pierwszą jest wodór H2',
+  ];
+
+  String getQuote() {
+    final _random = Random();
+    String quote = quotes[_random.nextInt(quotes.length)];
+    return quote;
+  }
+}
