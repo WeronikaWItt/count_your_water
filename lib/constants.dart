3438f51204671c5b76852e9ce6e9a09c9c5d4268
AlexandraHraudul <70907300+AlexandraHraudul@users.noreply.github.com>
2022-01-23 23:04:14 +0100

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,11 +4,11 @@ const kLightBlue = Color(0xff0288d1);
 const kBlue = Color(0xff01579B);
 List<String> quotes = [
   'Bądź jak woda – przystosowuje się do każdego naczynia, a może zniszczyć skałę.',
-  'Ludzie są jak morze, czasem łagodni i przyjaźni, czasem burzliwi i zdradliwi. Przede wszystkim to jednak tylko woda.',
-  'Żyjemy niczym woda spływająca z góry, podążając mniej więcej w tym samym kierunku, póki jakaś przeszkoda nie zmusi nas do poszukania nowego koryta.',
+  //'Ludzie są jak morze, czasem łagodni i przyjaźni, czasem burzliwi i zdradliwi. Przede wszystkim to jednak tylko woda.',
+  //'Żyjemy niczym woda spływająca z góry, podążając mniej więcej w tym samym kierunku, póki jakaś przeszkoda nie zmusi nas do poszukania nowego koryta.',
   'Bądź niczym bijące źródło, nie zaś jak staw, w którym zawsze stoi ta sama woda.',
   'Chcesz być zdrowy, młody, nie stroń nigdy od wody.',
-  'Wszystkie istoty ciągną do wody i wokół niej organizują swoje życie. Podobnie ludzkość. Sercem każdej społeczności jest woda: wielka jak morze albo mała jak źródełko.',
+  //'Wszystkie istoty ciągną do wody i wokół niej organizują swoje życie. Podobnie ludzkość. Sercem każdej społeczności jest woda: wielka jak morze albo mała jak źródełko.',
   'Woda pokrywa 70,9 procent powierzchni planety.',
   'W atmosferze jest więcej wody niż we wszystkich naszych rzekach razem wziętych.',
   'Meduza i ogórek to w 95% woda.',
